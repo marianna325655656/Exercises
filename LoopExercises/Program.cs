7474afca687a6c1501a154242615e725e671f842
marinaRaf <marianna.rafayelyan@gmail.com>
2023-10-19 11:27:28 +0400

    uncomment code

--- LoopExercises/Program.cs
+++ LoopExercises/Program.cs
@@ -1,30 +1,30 @@
-﻿//int x = 1;
-//int sum = 0;
-//while (x <= 100)
-//{
-//    sum = sum + x;
-//    x++;
-//}
-//Console.WriteLine("sum: " + sum);
+﻿int x = 1;
+int sum = 0;
+while (x <= 100)
+{
+    sum = sum + x;
+    x++;
+}
+Console.WriteLine("sum: " + sum);
 
-//int x1 = 1;
-//int sum1 = 0;
-//do
-//{
-//    sum1 = sum1 + x1;
-//    x1++;
+int x1 = 1;
+int sum1 = 0;
+do
+{
+    sum1 = sum1 + x1;
+    x1++;
 
-//}
-//while (x <= 100);
-//Console.WriteLine("sum: " + sum1);
+}
+while (x <= 100);
+Console.WriteLine("sum: " + sum1);
 
-//int sum2 = 0;
-//for (int i = 1; i <= 100; i++)
-//{
-//    sum2 = sum2 + i;
+int sum2 = 0;
+for (int i = 1; i <= 100; i++)
+{
+    sum2 = sum2 + i;
 
-//}
-//Console.WriteLine("sum: " + sum2);
+}
+Console.WriteLine("sum: " + sum2);
 
 int  SumOfNumbers (int minNumber, int maxNumber) 
 {

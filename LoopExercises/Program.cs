375aa4b37e508f239e927e1ab1192d190433eea2
marinaRaf <marianna.rafayelyan@gmail.com>
2023-10-19 11:26:46 +0400

    made changes to function

--- LoopExercises/Program.cs
+++ LoopExercises/Program.cs
@@ -1,42 +1,40 @@
-﻿int x = 1;
-int sum = 0;
-while (x <= 100)
-{
-    sum = sum + x;
-    x++;
-}
-Console.WriteLine("sum: " + sum);
+﻿//int x = 1;
+//int sum = 0;
+//while (x <= 100)
+//{
+//    sum = sum + x;
+//    x++;
+//}
+//Console.WriteLine("sum: " + sum);
 
-int x1 = 1;
-int sum1 = 0;
-do
-{
-    sum1 = sum1 + x1;
-    x1++;
+//int x1 = 1;
+//int sum1 = 0;
+//do
+//{
+//    sum1 = sum1 + x1;
+//    x1++;
 
-}
-while (x <= 100);
-Console.WriteLine("sum: " + sum1);
+//}
+//while (x <= 100);
+//Console.WriteLine("sum: " + sum1);
 
-int sum2 = 0;
-for (int i = 1; i <= 100; i++)
-{
-    sum2 = sum2 + i;
+//int sum2 = 0;
+//for (int i = 1; i <= 100; i++)
+//{
+//    sum2 = sum2 + i;
 
-}
-Console.WriteLine("sum: " + sum2);
+//}
+//Console.WriteLine("sum: " + sum2);
 
 int  SumOfNumbers (int minNumber, int maxNumber) 
 {
-    int sum = 0;    
+    int sum3 = 0;    
     for (int i= minNumber; i <= maxNumber; i++) 
     {
-        sum = sum + i;
+        sum3 = sum3 + i;
     }
-    return sum;
+    return sum3;
 }
 
 
-var sum3 = SumOfNumbers(1, 100);
-
-Console.WriteLine("sum3: " + sum3);
+Console.WriteLine("sum3: " + SumOfNumbers(1,100));
